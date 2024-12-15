23b8d4e93a52582d575eb56a0c355dc411da44e2
ZiZi07 <ashfakurriaz@gmail.com>
2024-12-16 01:07:08 +0530

    url update

--- src/utils/apiRequest.js
+++ src/utils/apiRequest.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const apiRequest = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:8080/api"
-      : "https://kid-network-server.onrender.com/api",
+  baseURL: "https://kid-network-server.onrender.com/api",
   withCredentials: true,
 });
